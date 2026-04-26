3b14c985f24d20c0583fe96dce1807bf4233f907
root <root@DESKTOP-NUC73FD.localdomain>
2026-04-26 09:31:03 +0100

    Update

--- src/builtins.c
+++ src/builtins.c
@@ -4,10 +4,7 @@ int	builtin_echo(t_shell *shell, t_cmd *cmd)
 {
 	int	i;
 	int	newline;
-	char	buf[4096];
-	int	n;
 
-	fprintf(stderr, "[DEBUG] builtin_echo called\n");
 	i = 1;
 	newline = 1;
 	if (cmd->argc > 1 && ft_strcmp(cmd->args[1], "-n") == 0)
@@ -25,12 +22,6 @@ int	builtin_echo(t_shell *shell, t_cmd *cmd)
 	if (newline)
 		printf("\n");
 	fflush(stdout);
-	fprintf(stderr, "[DEBUG] about to read from stdin\n");
-	while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
-	{
-		fprintf(stderr, "[DEBUG] read %d bytes\n", n);
-	}
-	fprintf(stderr, "[DEBUG] finished reading\n");
 	return (0);
 	(void)shell;
 }
@@ -40,6 +31,11 @@ int	builtin_cd(t_shell *shell, t_cmd *cmd)
 	char	*path;
 	char	cwd[4096];
 
+	if (cmd->argc > 2)
+	{
+		fprintf(stderr, "minishell: cd: too many arguments\n");
+		return (1);
+	}
 	if (cmd->argc == 1)
 		path = get_env(shell, "HOME");
 	else
@@ -73,23 +69,73 @@ int	builtin_export(t_shell *shell, t_cmd *cmd)
 	char	*equal_sign;
 	char	*key;
 	char	*value;
+	int		j;
+	int		has_error;
 
 	i = 1;
+	has_error = 0;
 	while (i < cmd->argc)
 	{
 		equal_sign = ft_strchr(cmd->args[i], '=');
 		if (equal_sign)
 		{
 			key = ft_strndup(cmd->args[i], equal_sign - cmd->args[i]);
+			if (!key || *key == '\0' || !ft_isalpha(key[0]))
+			{
+				fprintf(stderr, "minishell: export: '%s': not a valid identifier\n", 
+					cmd->args[i]);
+				free(key);
+				has_error = 1;
+				i++;
+				continue;
+			}
+			j = 1;
+			while (key[j])
+			{
+				if (!ft_isalnum(key[j]) && key[j] != '_')
+				{
+					fprintf(stderr, "minishell: export: '%s': not a valid identifier\n", 
+						cmd->args[i]);
+					free(key);
+					has_error = 1;
+					i++;
+					goto next_export;
+				}
+				j++;
+			}
 			value = equal_sign + 1;
 			set_env(shell, key, value);
 			free(key);
 		}
 		else
+		{
+			if (*cmd->args[i] == '\0' || !ft_isalpha(cmd->args[i][0]))
+			{
+				fprintf(stderr, "minishell: export: '%s': not a valid identifier\n", 
+					cmd->args[i]);
+				has_error = 1;
+				i++;
+				continue;
+			}
+			j = 1;
+			while (cmd->args[i][j])
+			{
+				if (!ft_isalnum(cmd->args[i][j]) && cmd->args[i][j] != '_')
+				{
+					fprintf(stderr, "minishell: export: '%s': not a valid identifier\n", 
+						cmd->args[i]);
+					has_error = 1;
+					i++;
+					goto next_export;
+				}
+				j++;
+			}
 			set_env(shell, cmd->args[i], "");
+		}
+next_export:
 		i++;
 	}
-	return (0);
+	return (has_error);
 }
 
 int	builtin_unset(t_shell *shell, t_cmd *cmd)
@@ -138,11 +184,42 @@ int	builtin_env(t_shell *shell, t_cmd *cmd)
 
 int	builtin_exit(t_shell *shell, t_cmd *cmd)
 {
-	int	exit_code;
+	int		exit_code;
+	int		i;
+	char	*str;
 
 	exit_code = 0;
+	if (cmd->argc > 2)
+	{
+		fprintf(stderr, "minishell: exit: too many arguments\n");
+		shell->exit_status = 1;
+		return (1);
+	}
 	if (cmd->argc > 1)
+	{
+		str = cmd->args[1];
+		if (*str == '+' || *str == '-')
+			str++;
+		if (*str == '\0')
+		{
+			fprintf(stderr, "minishell: exit: %s: numeric argument required\n", 
+				cmd->args[1]);
+			exit(2);
+		}
+		i = 0;
+		while (str[i])
+		{
+			if (!ft_isdigit(str[i]))
+			{
+				fprintf(stderr, "minishell: exit: %s: numeric argument required\n", 
+					cmd->args[1]);
+				exit(2);
+			}
+			i++;
+		}
 		exit_code = atoi(cmd->args[1]);
-	(void)shell;
+	}
+	free_shell(shell);
 	exit(exit_code);
+	return (0);
 }
--- src/utils.c
+++ src/utils.c
@@ -455,6 +455,16 @@ int	ft_isalnum(int c)
 	return ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
 
+int	ft_isalpha(int c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
 char	*ft_itoa(int n)
 {
 	char	*result;
